e1a5bccf26b7fc18ec52427b641e727dae9a0ec4
Deepanshugupta23 <deepanshug3512@gmail.com>
2026-03-06 17:44:48 +0530

    My_dark

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./App.css";
 import {  Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./Pages/Home";
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/My_Quiz_Platform20">
     <App />
 
     </BrowserRouter>
